a5070aad6132309d89037a4c5cc353ac557c4c5f
shyamraj <shyamraj@qburst.com>
2017-02-06 11:31:30 +0530

    rmvd role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def Admins_authorise
    current_user
-   if current_user.email == "admin@prismclub.com"
-    current_user.update_attribute(:role, "admin")
-   end
    if current_user != nil and current_user.role == "admin"
       puts "LOG IN"
      
